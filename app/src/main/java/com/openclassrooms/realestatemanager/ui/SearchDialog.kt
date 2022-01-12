5bcefbf1a86c0c171c1cf684f0022b3592d895bb
OurdiaD <daideche.o@gmail.com>
2022-01-12 13:20:17 +0100

    interests search

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/SearchDialog.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/SearchDialog.kt
@@ -2,6 +2,8 @@ package com.openclassrooms.realestatemanager.ui
 
 import android.app.Dialog
 import android.os.Bundle
+import android.text.BoringLayout
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.openclassrooms.realestatemanager.R
@@ -11,6 +13,22 @@ import com.openclassrooms.realestatemanager.databinding.DialogSearchBinding
 class SearchDialog : DialogFragment() {
 
     private lateinit var binding: DialogSearchBinding
+    var condition = false
+
+    companion object {
+        var minPrice: String? = null
+        var maxPrice: String? = null
+        var minSurface: String? = null
+        var maxSurface: String? = null
+        var minRoom: String? = null
+        var maxRoom: String? = null
+        var health: Boolean = false
+        var school: Boolean = false
+        var market: Boolean = false
+        var park: Boolean = false
+        var restaurant: Boolean = false
+        var transports: Boolean = false
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = DialogSearchBinding.inflate(layoutInflater)
@@ -27,51 +45,41 @@ class SearchDialog : DialogFragment() {
                     R.string.cancel) { dialog, id ->
                     this.dismiss()
                 }
+            putLastValues()
+            setInterests()
             builder.create()
+
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
     fun getParam(){
-        val minPrice = binding.searchMinPriceEdit.text.toString()
-        val maxPrice = binding.searchMaxPriceEdit.text.toString()
-        val minSurface = binding.searchMinSurfaceEdit.text.toString()
-        val maxSurface = binding.searchMaxSurfaceEdit.text.toString()
-        val minRoom = binding.searchMinRoomsEdit.text.toString()
-        val maxRoom = binding.searchMaxRoomsEdit.text.toString()
+        minPrice = binding.searchMinPriceEdit.text.toString()
+        maxPrice = binding.searchMaxPriceEdit.text.toString()
+        minSurface = binding.searchMinSurfaceEdit.text.toString()
+        maxSurface = binding.searchMaxSurfaceEdit.text.toString()
+        minRoom = binding.searchMinRoomsEdit.text.toString()
+        maxRoom = binding.searchMaxRoomsEdit.text.toString()
+        health = binding.searchHealth.isChecked
+        school = binding.searchSchool.isChecked
+        market = binding.searchMarket.isChecked
+        transports = binding.searchTransports.isChecked
+        restaurant = binding.searchRestaurant.isChecked
+        park = binding.searchPark.isChecked
 
         var query = ""
         var baseQuery = "SELECT * FROM property"
-        var condition = false
-
-        if(minPrice != "") {
-            query += getQueryIntSupTo("price", minPrice, condition)
-            condition = true
-        }
-
-        if(maxPrice != "") {
-            query += getQueryIntInfTo("price", maxPrice,condition)
-            condition = true
-        }
-
-        if (minSurface != "") {
-            query += getQueryIntSupTo("surfaceArea", minSurface, condition)
-            condition = true
-        }
-
-        if (maxSurface != "") {
-            query += getQueryIntSupTo("surfaceArea", maxSurface, condition)
-            condition = true
-        }
-
-        if (minRoom != "") {
-            query += getQueryIntSupTo("numberOfRooms", minRoom, condition)
-            condition = true
-        }
-
-        if (maxRoom != "") {
-            query += getQueryIntInfTo("numberOfRooms", maxRoom, condition)
-            condition = true
-        }
+        query += getQueryIntSupTo("price", minPrice!!)
+        query += getQueryIntInfTo("price", maxPrice!!)
+        query += getQueryIntSupTo("surfaceArea", minSurface!!)
+        query += getQueryIntSupTo("surfaceArea", maxSurface!!)
+        query += getQueryIntSupTo("numberOfRooms", minRoom!!)
+        query += getQueryIntInfTo("numberOfRooms", maxRoom!!)
+        query += getQueryBool("health", health)
+        query += getQueryBool("school", school)
+        query += getQueryBool("market", market)
+        query += getQueryBool("transports", transports)
+        query += getQueryBool("restaurant", restaurant)
+        query += getQueryBool("park", park)
 
         if (condition) {
             baseQuery += " WHERE $query"
@@ -81,18 +89,54 @@ class SearchDialog : DialogFragment() {
 
     }
 
-    fun getQueryIntSupTo(field: String, value: String, condition: Boolean): String {
+    fun getQueryIntSupTo(field: String, value: String): String {
         var query = ""
-        if (condition) query = " AND"
-        query = "$query $field > $value"
+        if (value != ""){
+            if (condition) query = " AND"
+            query = "$query $field > $value"
+            condition = true
+        }
         return query
     }
 
-    fun getQueryIntInfTo(field: String, value: String, condition: Boolean): String {
+    fun getQueryIntInfTo(field: String, value: String): String {
         var query = ""
-        if (condition) query = " AND"
-        query = " $query $field < $value"
+        if (value != ""){
+            if (condition) query = " AND"
+            query = " $query $field < $value"
+            condition = true
+        }
         return query
     }
+
+    fun getQueryBool(field: String, value: Boolean): String {
+        var query = ""
+        if (value){
+            if (condition) query = " AND"
+            query = " $query $field = 1"
+            condition = true
+        }
+        return query
+    }
+
+    fun putLastValues() {
+        binding.searchMinPriceEdit.setText(minPrice)
+        binding.searchMaxPriceEdit.setText(maxPrice)
+        binding.searchMinSurfaceEdit.setText(minSurface)
+        binding.searchMaxSurfaceEdit.setText(maxSurface)
+        binding.searchMinRoomsEdit.setText(minRoom)
+        binding.searchMaxRoomsEdit.setText(maxRoom)
+    }
+
+    fun setInterests() {
+        binding.searchHealth.isChecked = health
+        binding.searchSchool.isChecked = school
+        binding.searchMarket.isChecked = market
+        binding.searchTransports.isChecked = transports
+        binding.searchRestaurant.isChecked = restaurant
+        binding.searchPark.isChecked = park
+
+    }
+
 
 }
